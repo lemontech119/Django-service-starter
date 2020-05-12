90661491e589bbd629935bf52c86465cad88ec99
Paik Gyeongjun <paik625@naver.com>
2020-05-12 22:29:37 +0900

    fix err

--- config/settings/development.py
+++ config/settings/development.py
@@ -20,18 +20,18 @@ INTERNAL_IPS = ['127.0.0.1']
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-with open('env/etc/db.txt') as db_info:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': db_info.read().strip(),
-            'USER': db_info.read().strip(),
-            'PASSWORD': db_info.read().strip(),
-            'HOST': db_info.read().strip(),
-            'PORT': db_info.read().strip(),
-            'ATOMIC_REQUESTS': True,
-        }
-    }
+# with open('env/etc/db.txt') as db_info:
+#     DATABASES = {~
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': db_info.readline().strip(),
+#             'USER': db_info.readline().strip(),
+#             'PASSWORD': db_info.readline().strip(),
+#             'HOST': db_info.readline().strip(),
+#             'PORT': db_info.readline().strip(),
+#             'ATOMIC_REQUESTS': True,
+#         }
+#     }
 
 # enable http oauth token transport in requests-oauthlib
 # WARNING: only set for dev env
